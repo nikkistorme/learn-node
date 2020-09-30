2bc7efc47e05221836f0f6656ee2ca9364189aa1
Nicholas Bright <nikkistorme@gmail.com>
2020-09-29 20:50:14 -0400

    finished Core Concept - Routing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  // const nikki = { name: 'Nikki', age: 25, cool: true };
+  // res.send('Hey! It works!');
+  // res.json(nikki);
+  res.json(req.query)
 });
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
+})
 
 module.exports = router;
